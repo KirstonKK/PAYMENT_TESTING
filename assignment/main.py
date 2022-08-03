3d52e66390f74bab80aa9422bfc9fde54660b78d
KirstonKK <88221941+KirstonKK@users.noreply.github.com>
2022-08-02 18:07:04 -0700

    Point of Sale System

---
+++ assignment/main.py
@@ -0,0 +1,16 @@
+# point of sale system 
+
+from pay.order import LineItem, Order
+from pay.payment import pay_order
+
+
+def main():
+    # Test card number: 1249190007575069
+    order = Order()
+    order.line_items.append(LineItem(name="banku", price=100_00, quantity=2))
+    order.line_items.append(LineItem(name="fufu", price=50_00))
+    pay_order(order)
+
+
+if __name__ == "__main__":
+    main()
