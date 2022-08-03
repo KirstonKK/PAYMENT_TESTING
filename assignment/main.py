91fadfdf587fa147a49f292dbc4789186b74afff
KirstonKK <88221941+KirstonKK@users.noreply.github.com>
2022-08-03 16:18:00 -0700

    Update main.py

--- assignment/main.py
+++ assignment/main.py
@@ -8,7 +8,7 @@ def main():
     # Test card number: 1249190007575069
     order = Order()
     order.line_items.append(LineItem(name="banku", price=200_00, quantity=2))
-    order.line_items.append(LineItem(name="fufu", price=10_00))
+    order.line_items.append(LineItem(name="fufu", price=50_00))
     pay_order(order)
 
 
