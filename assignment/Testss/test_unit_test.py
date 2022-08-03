79806250c0653a8842d679dbbc303d10edfe328a
KirstonKK <88221941+KirstonKK@users.noreply.github.com>
2022-08-02 19:09:13 -0700

    Update test_unit_test.py

--- assignment/Testss/test_unit_test.py
+++ assignment/Testss/test_unit_test.py
@@ -1,5 +1,7 @@
 import unittest
 from pay.processor import PaymentProcessor
+from pay.order import LineItem
+from pay.order import LineItem, Order
 
 API_KEY = "6cfb67f3-6281-4031-b893-ea85db0dce20"
 
@@ -39,3 +41,26 @@ def test_charge_card_invalid():
     with unittest.raises(ValueError):
         payment_processor = PaymentProcessor(API_KEY)
         payment_processor.charge("1249190007575068", 8, 2026, 100)
+
+
+def test_line_item_total() -> None:
+    line_item = LineItem(name="Test", price=100)
+    assert line_item.total == 100
+
+
+def test_line_item_total_quantity() -> None:
+    line_item = LineItem(name="Test", price=100, quantity=2)
+    assert line_item.total == 200
+
+
+def test_empty_order_total() -> None:
+    order = Order()
+    assert order.total == 0
+
+
+def test_order_total() -> None:
+    order = Order()
+    order.line_items.append(LineItem(name="Test", price=100))
+    assert order.total == 100
+
+
